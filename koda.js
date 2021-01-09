9a30c19f6e9b02f8d5ec4295ed535fc346b18588
PolnerFilip <filip.polner@student.um.si>
2021-01-09 18:42:40 +0100

    prvi

--- koda.js
+++ koda.js
@@ -1,3 +1,20 @@
-function a () {
+function funkcija() {
+    //console.log("halo");
+    $.ajax({
+        url: 'https://api.sledilnik.org/api/stats',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            console.log(data.length);
+            var json = data[data.length - 1];
 
+            var testidanes = json.performed.today;
+            var pozitivnidanes = json.cases.confirmedToday;
+            var hospitaliziranidanes = json.statePerTreatment.inHospital;
+            var umrlidanes = json.statePerTreatment.deceased;
+            var cepljenidanes = json.vaccination.administered.today;
+
+            document.getElementById("testidanes").innerHTML = testidanes;
+        }
+    });
 }
