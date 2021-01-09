4283a506c5df3d4e5f1e504df732887790f61f1f
PolnerFilip <filip.polner@student.um.si>
2021-01-09 18:56:48 +0100

    drugi

--- koda.js
+++ koda.js
@@ -6,15 +6,33 @@ function funkcija() {
         dataType: 'json',
         success: function(data) {
             console.log(data.length);
-            var json = data[data.length - 1];
+            var json;
+            if (data[data.length - 1].hasOwnProperty('performed.today')) {
+                json = data[data.length - 1];
+            } else {
+                json = data[data.length - 2]
+            }
 
-            var testidanes = json.performed.today;
+            console.log(json);
+            var dan = json.day;
+            var mesec = json.month;
+            var leto = json.year;
+
+            var izpis = 'Podatki za: ' + dan + '.' + mesec + '.' + leto;
+            document.getElementById("datum").innerHTML = izpis;
+
+            var testidanes = json.tests.performed.today;
+            console.log(testidanes);
             var pozitivnidanes = json.cases.confirmedToday;
             var hospitaliziranidanes = json.statePerTreatment.inHospital;
             var umrlidanes = json.statePerTreatment.deceased;
             var cepljenidanes = json.vaccination.administered.today;
 
-            document.getElementById("testidanes").innerHTML = testidanes;
+            document.getElementById("testiranjadanes").innerHTML = testidanes;
+            document.getElementById("pozitivnidanes").innerHTML = pozitivnidanes;
+            document.getElementById("hospitaliziranidanes").innerHTML = hospitaliziranidanes;
+            document.getElementById("umrlidanes").innerHTML = umrlidanes;
+            document.getElementById("cepljenidanes").innerHTML = cepljenidanes;
         }
     });
 }
